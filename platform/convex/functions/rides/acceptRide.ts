5d6220da33229789cc49c5be82c807fa5c3ef129
un4thi <u22553402@tuks.co.za>
2025-06-04 16:32:26 +0200

    creating a function for accepting a ride

---
+++ platform/convex/functions/rides/acceptRide.ts
@@ -0,0 +1,3 @@
+//i am hoping to write a function that accepts a ride request and updates the ride status in the database,as well as assigns a driver to that ride.
+//have to change the ride status to "accepted" and assign the driver to the ride.
+//need to change rides table in schema to include driverId and status
