74c17aa4573d48ae6e84a39aa7d681ab11a12504
un4thi <u22553402@tuks.co.za>
2025-06-07 17:11:53 +0200

    creating the function for a user to cancel a ride

---
+++ platform/convex/functions/rides/cancelRide.ts
@@ -0,0 +1,23 @@
+//this os the funtionc for a user to cancel a ride
+import { mutation } from "../../_generated/server";
+import { v } from "convex/values";
+export const cancelRide = mutation({
+    args: {
+        rideId: v.string(),
+        userId: v.id("taxiTap_users"),
+    },
+    handler: async (ctx, args) => {
+        // Find the ride
+        const ride = await ctx.db
+            .query("rides")
+            .withIndex("by_ride_id", (q) => q.eq("rideId", args.rideId))
+            .unique();
+
+        if (!ride) {
+            throw new Error("Ride not found");
+        }
+
+        // Check if the user is authorized to cancel the ride
+        if (ride.passengerId !== args.userId) {
+            throw new Error("You are not authorized to cancel this ride");
+        }
