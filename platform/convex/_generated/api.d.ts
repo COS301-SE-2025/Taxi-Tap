3008234efc2b15bfe001ff2aad7dd5adc74a5355
Ann-Mari-Oberholzer <167811099+Ann-Mari-Oberholzer@users.noreply.github.com>
2025-06-10 14:28:04 +0200

    Change eslint file and delete all files not needed anymore.

--- platform/convex/_generated/api.d.ts
+++ platform/convex/_generated/api.d.ts
@@ -14,12 +14,10 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as functions_family_addChild from "../functions/family/addChild.js";
-import type * as functions_family_idkAddChildHandler from "../functions/family/idkAddChildHandler.js";
 import type * as functions_rides_RequestRide from "../functions/rides/RequestRide.js";
 import type * as functions_users_UserManagement_logInWithEmail from "../functions/users/UserManagement/logInWithEmail.js";
 import type * as functions_users_UserManagement_signUpWithEmail from "../functions/users/UserManagement/signUpWithEmail.js";
 import type * as functions_users_addUser from "../functions/users/addUser.js";
-import type * as functions_users_addUserHandler from "../functions/users/addUserHandler.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -31,12 +29,10 @@ import type * as functions_users_addUserHandler from "../functions/users/addUser
  */
 declare const fullApi: ApiFromModules<{
   "functions/family/addChild": typeof functions_family_addChild;
-  "functions/family/idkAddChildHandler": typeof functions_family_idkAddChildHandler;
   "functions/rides/RequestRide": typeof functions_rides_RequestRide;
   "functions/users/UserManagement/logInWithEmail": typeof functions_users_UserManagement_logInWithEmail;
   "functions/users/UserManagement/signUpWithEmail": typeof functions_users_UserManagement_signUpWithEmail;
   "functions/users/addUser": typeof functions_users_addUser;
-  "functions/users/addUserHandler": typeof functions_users_addUserHandler;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
