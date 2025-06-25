17dcc1714ad667de053832c0c776fd34792f023c
Nevan Sami Rahman <133259091+rsnevan@users.noreply.github.com>
2025-06-25 15:53:55 +0200

    Fixing StartRide

--- platform/convex/functions/taxis/viewTaxiInfo.ts
+++ platform/convex/functions/taxis/viewTaxiInfo.ts
@@ -57,6 +57,7 @@ export async function viewTaxiInfoHandler(ctx: QueryCtx, args: { passengerId: Id
       name: driverUser?.name,
       phoneNumber: driverUser?.phoneNumber,
       rating: driverProfile.averageRating,
+      userId: driverUser?._id,
     },
     rideId: ride.rideId,
     rideDocId: ride._id,
